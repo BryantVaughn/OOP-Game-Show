96e0cb20373b53639ac41b2b22df3f6bdd33ec0d
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-12-21 12:44:30 -0700

    Add addPhraseToDisplay method to Phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -2,4 +2,22 @@ class Phrase {
 	constructor(phrase) {
 		this.phrase = phrase;
 	}
+
+	/**
+	 * Renders blank phrase to screen.
+	 */
+	addPhraseToDisplay() {
+		const phraseUL = document.querySelector('#phrase > ul');
+
+		for (let char of this.phrase) {
+			const listItem = document.createElement('li');
+			if (!char === ' ') {
+				listItem.className = `hide letter ${char}`;
+			} else {
+				listItem.className = 'space';
+			}
+			listItem.textContent = `${char}`;
+			phraseUL.appendChild(listItem);
+		}
+	}
 }
