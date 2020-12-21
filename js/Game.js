abfe42a9b255f487bbf55c897be2e22549c7f0b6
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-12-21 14:50:39 -0700

    Add method comments and gameOver method for Game class

--- js/Game.js
+++ js/Game.js
@@ -38,6 +38,11 @@ class Game {
 		return this.phrases[rand];
 	}
 
+	/**
+	 * Checks if user's selected letter is in the phrase or not,
+	 * then checks if the game has been won or lost.
+	 * @param {object} e - Event object for user's clicked letter.
+	 */
 	handleInteraction(e) {
 		e.target.disabled = true;
 
@@ -47,7 +52,7 @@ class Game {
 		if (isFound) {
 			e.target.classList.add('chosen');
 			this.activePhrase.showMatchedLetter(letter);
-			if (this.checkForWin()) gameOver();
+			if (this.checkForWin()) gameOver(true);
 		} else {
 			e.target.classList.add('wrong');
 			this.removeLive();
@@ -62,7 +67,7 @@ class Game {
 		lifeHearts[this.missed].src = 'images/lostHeart.png';
 		this.missed += 1;
 
-		if (this.missed > 4) this.gameOver();
+		if (this.missed > 4) this.gameOver(false);
 	}
 
 	/**
@@ -76,5 +81,22 @@ class Game {
 			if (item.className.includes('hide')) return false;
 		});
 		return true;
+	}
+
+	/**
+	 * Displays the overlay and provides a win or lose message for user.
+	 * @param {boolean} result - Win (true) or lose (false).
+	 */
+	gameOver(result) {
+		const overlay = document.getElementById('overlay');
+		overlay.classList.remove('start');
+
+		if (result) {
+			overlay.querySelector('#game-over-message').textContent = 'You win!!!';
+			overlay.classList.add('win');
+		} else {
+			overlay.querySelector('#game-over-message').textContent = 'You lose...';
+			overlay.classList.add('lose');
+		}
 	}
 }
