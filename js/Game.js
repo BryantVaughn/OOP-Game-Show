a246e28d3511196250582ce8918af4ebae20468c
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-12-21 13:13:33 -0700

    Add Game class with constructor and initializePhrases method

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,22 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+class Game {
+	constructor() {
+		this.missed = 0;
+		this.phrases = this.initializePhrases();
+		this.activePhrase = null;
+	}
+
+	/**
+	 * Creates 5 Phrase objects.
+	 * @return {Array} phrases - An Array of 5 Phrase objects.
+	 */
+	initializePhrases() {
+		const phrases = [];
+		phrases.push(new Phrase('a dime a dozen'));
+		phrases.push(new Phrase('beating around the bush'));
+		phrases.push(new Phrase('i lost track of time'));
+		phrases.push(new Phrase('actions speak louder than words'));
+		phrases.push(new Phrase('i can live with that'));
+
+		return phrases;
+	}
+}
