2bd0d5f040aa0f6ebe0738eecbb5b6aa99ce4f1b
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-12-22 10:03:52 -0700

    Add comments to app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
+// Gather DOM elements
 const startBtn = document.querySelector('#btn__reset');
 const keyboard = document.querySelector('#qwerty');
 
+// Create game variable for access in event listeners
 let game;
 
+// Event listeners
+// Start button click listener
 startBtn.addEventListener('click', () => {
 	game = new Game();
 	game.startGame();
 });
 
+// Keyboard click listener
 keyboard.addEventListener('click', (evt) => {
 	if (evt.target.tagName === 'BUTTON') {
 		game.handleInteraction(evt);
